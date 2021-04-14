5b6c87ec1e8a35e041c8d3fd74c6f60555e6d733
Jerico22 <77520650+Jerico22@users.noreply.github.com>
2021-04-14 14:10:19 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! I have been at Cal Poly for 3 years");
+        System.out.println("Hello, World! I have been at Cal Poly for 3 years and I'm passing all my classes with A's");
     }
 }
